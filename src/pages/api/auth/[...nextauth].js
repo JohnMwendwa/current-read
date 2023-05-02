6e5553397594fc23ac2ca7953082f82f4eb959ac
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-02 19:48:58 +0300

    setup email provider

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,24 @@
+import NextAuth from "next-auth";
+import EmailProvider from "next-auth/providers/email";
+
+import { sendVerificationRequest } from "../../../lib/emails/send-verification";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    EmailProvider({
+      server: {
+        host: process.env.EMAIL_SERVER_HOST,
+        port: process.env.EMAIL_SERVER_PORT,
+        auth: {
+          user: process.env.EMAIL_SERVER_USER,
+          pass: process.env.EMAIL_SERVER_PASSWORD,
+        },
+      },
+      from: process.env.EMAIL_FROM,
+    }),
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
