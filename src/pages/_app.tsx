e0f8bdbd5cd73bb800d912b4d2a63ef410677994
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-02 19:52:41 +0300

    wrap the whole app with the sesion prvider component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,14 @@
 import { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 
 import "../styles/globals.css";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
