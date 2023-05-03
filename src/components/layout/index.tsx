f4f5c9a85b67ad47e729bb48deced689cce2e33b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-03 18:15:26 +0300

    Export the default layout component

---
+++ src/components/layout/index.tsx
@@ -0,0 +1,14 @@
+import Nav from "./Nav";
+import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Nav />
+      <main>{children}</main>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
