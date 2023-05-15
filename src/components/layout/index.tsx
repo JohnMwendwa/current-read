e155b02a538e137c77b7e0ddbcad73eefdc6c1cb
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-16 00:08:52 +0300

    style the layout

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -3,9 +3,9 @@ import Footer from "./Footer";
 
 const Layout = ({ children }) => {
   return (
-    <div>
+    <div className="flex flex-col min-h-screen">
       <Nav />
-      <main>{children}</main>
+      <main className="flex-1 p-4">{children}</main>
       <Footer />
     </div>
   );
