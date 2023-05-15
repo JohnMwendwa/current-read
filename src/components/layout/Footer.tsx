8229cf0d92466d82f312638ab6b44302a02c5cc9
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-16 00:10:16 +0300

    style the footer

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 const Footer = () => {
   return (
-    <footer>
+    <footer className="text-center p-4 bg-gray-400">
       <p>
         &copy; {new Date().getFullYear()}{" "}
         <Link href={"https://johnmwendwa.me"}>John Mwendwa</Link>
