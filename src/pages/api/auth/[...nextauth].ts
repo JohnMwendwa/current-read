9167421cfe920b2b46d9538bd875d164d8f910f2
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-15 17:42:06 +0300

    use Credentias provider for authentication

---
+++ src/pages/api/auth/[...nextauth].ts
@@ -0,0 +1,37 @@
+import NextAuth from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+import connectDB from "lib/db/connection";
+import User from "lib/db/models/user_model";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      type: "credentials",
+      credentials: {
+        email: { label: "Username", type: "text", placeholder: "username" },
+        password: { label: "Password", type: "password" },
+      },
+
+      async authorize(credentials, req) {
+        await connectDB();
+
+        const user = await User.findByCredentials(
+          credentials.email,
+          credentials.password
+        );
+
+        return {
+          email: user.email,
+          image: "",
+          name: user.firstName,
+          id: user._id.toString(),
+        };
+      },
+    }),
+  ],
+};
+
+export default NextAuth(authOptions);
