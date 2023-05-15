4137b561944bddec16745c0c46ee26fb6c1030f3
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-15 13:39:02 +0300

    Add a static method for verifying user credentials

--- src/lib/db/models/user_model.ts
+++ src/lib/db/models/user_model.ts
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
+import { verifyPassword } from "../../passwords";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
   firstName: {
     type: String,
-    required: [true, "First Name is required"],
+    required: [true, "First name is required"],
   },
   lastName: {
     type: String,
@@ -19,6 +20,33 @@ const userSchema = new Schema({
     required: [true, "Password is required"],
   },
 });
+
+// Add a custom function for verifying user credentials
+userSchema.statics.findByCredentials = async (
+  email: string,
+  password: string
+) => {
+  const user = await User.findOne({ email });
+
+  //   If email doesn't exist throw an error
+  if (!user) {
+    throw new Error("Invalid Credentials");
+  }
+
+  // compare the password with the one in the DB
+  const match = await verifyPassword(password, user.password);
+
+  //   If passwords don't match throw an error
+  if (!match) {
+    throw new Error("Invalid Credentials");
+  }
+
+  const userObject = user.toObject();
+
+  //Remove user password from the returned string
+  delete userObject.password;
+  return userObject;
+};
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
 
