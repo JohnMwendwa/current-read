04a43428f48157e040e6989550ec059752b291e6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-15 12:56:53 +0300

    Create and export user schema

---
+++ src/lib/db/models/user_model.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  firstName: {
+    type: String,
+    required: [true, "First Name is required"],
+  },
+  lastName: {
+    type: String,
+    required: [true, "Last name is required"],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required"],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required"],
+  },
+});
+
+const User = mongoose.models.User || mongoose.model("User", userSchema);
+
+export default User;
