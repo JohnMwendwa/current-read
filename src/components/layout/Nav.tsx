48d1c1fa3896f3d7dd7b565cf03a0f97ce85b6f5
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-16 01:01:31 +0300

    stye the navbar

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -1,12 +1,29 @@
 import React from "react";
 import Link from "next/link";
-
+import { signIn, signOut, useSession } from "next-auth/react";
 const Nav = () => {
+  const { data: session } = useSession();
   return (
-    <header>
-      <Link href="/">JM</Link>
-      <nav>
-        <Link href={"/api/auth/signin"}>Login</Link>
+    <header className="flex justify-between items-center px-4 py-6 bg-blue-700 text-white">
+      <Link href="/" className="font-extrabold text-2xl">
+        JM
+      </Link>
+      <nav className="flex items-center">
+        {session?.user ? (
+          <button
+            onClick={() => signOut()}
+            className="bg-red-600 px-4 py-2 rounded-lg hover:bg-red-700"
+          >
+            Logout
+          </button>
+        ) : (
+          <button
+            onClick={() => signIn()}
+            className="bg-black px-4 py-2 rounded-lg hover:bg-gray-900"
+          >
+            Login
+          </button>
+        )}
       </nav>
     </header>
   );
