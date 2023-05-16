54792bdba6eed1f02770dce901c004b8fb92a8c3
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-16 14:35:18 +0300

    style the AddBookForm component

--- src/components/AddBookForm.tsx
+++ src/components/AddBookForm.tsx
@@ -2,14 +2,29 @@ import React from "react";
 
 const AddBookForm = () => {
   return (
-    <form>
-      <label htmlFor="title">
-        <input type="text" id="title" />
+    <form className=" flex-1 flex flex-col items-center justify-center w-full">
+      <label htmlFor="title" className="max-w-sm w-full">
+        <input
+          type="text"
+          id="title"
+          placeholder="Book Title"
+          className="text-center rounded-md my-2 p-2 w-full border border-gray-700"
+        />
       </label>
-      <label htmlFor="author">
-        <input type="text" id="author" />
+      <label htmlFor="author" className="max-w-sm w-full">
+        <input
+          type="text"
+          id="author"
+          placeholder="author name"
+          className="text-center rounded-md my-2 p-2 w-full border border-gray-700"
+        />
       </label>
-      <button type="submit">Add New Book</button>
+      <button
+        type="submit"
+        className="max-w-sm w-full bg-green-600 text-white px-4 py-2 rounded-md"
+      >
+        Add New Book
+      </button>
     </form>
   );
 };
