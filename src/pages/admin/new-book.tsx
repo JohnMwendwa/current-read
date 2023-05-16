803adea4d11bd1da1bfd4c9dd2731c65aa1bc9e8
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-16 14:20:12 +0300

    Add a link to go back to the index admin page

--- src/pages/admin/new-book.tsx
+++ src/pages/admin/new-book.tsx
@@ -1,9 +1,18 @@
 import AddBookForm from "components/AddBookForm";
+import Link from "next/link";
 
 const NewBookPage = () => {
   return (
     <div className="flex flex-col items-center">
       <AddBookForm />
+      <div className="mt-auto">
+        <Link
+          href="/admin"
+          className="bg-black text-white px-4 py-2 rounded-md"
+        >
+          Back
+        </Link>
+      </div>
     </div>
   );
 };
