ba7ba80a63818677d3365f06ffd22c011877f278
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-18 00:48:16 +0300

    style the index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import BookList from "components/BookList";
 export default function Home() {
   return (
     <div>
-      <h1>Current Read</h1>
+      <h1 className="text-2xl text-center font-bold mb-4">My Reading List</h1>
       <BookList />
     </div>
   );
