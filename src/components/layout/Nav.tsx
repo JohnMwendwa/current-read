119ccac663ae02790ecbb652ce7d1dcc1a93cc10
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-18 21:19:30 +0300

    style navbar

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -4,7 +4,7 @@ import { signIn, signOut, useSession } from "next-auth/react";
 const Nav = () => {
   const { data: session } = useSession();
   return (
-    <header className="flex justify-between items-center px-4 py-6 bg-blue-700 text-white">
+    <header className="flex justify-between items-center px-6 py-4 bg-blue-700/80 text-white shadow-md ">
       <Link href="/" className="font-extrabold text-2xl">
         JM
       </Link>
@@ -19,7 +19,7 @@ const Nav = () => {
         ) : (
           <button
             onClick={() => signIn()}
-            className="bg-black px-4 py-2 rounded-lg hover:bg-gray-900"
+            className="bg-gray-900 px-4 py-2 rounded-lg hover:bg-cyan-900"
           >
             Login
           </button>
