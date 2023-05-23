83805f4c114c4262c1e31818c7c06f573dfc6c7b
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-24 02:34:10 +0300

    style link

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -14,7 +14,7 @@ const AdminPage = () => {
     <div className="flex flex-col items-center">
       <Link
         href="/admin/new-book"
-        className="bg-green-600 text-white px-4 py-2 rounded-lg hover:bg-green-700 mb-4"
+        className="bg-green-600 text-white px-4 pt-1 pb-2 rounded-lg hover:bg-green-700 mb-4"
       >
         <span className="text-2xl mr-2">+</span>
         Add New Book
