c24b8d4d524f1a30f99d1c039f12bec330dd0704
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-24 21:11:41 +0300

    Create api logic for updating book title or author

--- src/pages/api/books/update.ts
+++ src/pages/api/books/update.ts
@@ -1,22 +1,23 @@
 import { getServerSession } from "next-auth/next";
+import { NextApiRequest, NextApiResponse } from "next";
 
 import connectDB from "lib/db/connection";
 import Book from "lib/db/models/book_model";
 import { authOptions } from "../auth/[...nextauth]";
 
-const handler = async (req, res) => {
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const session = await getServerSession(req, res, authOptions);
+
+  // Check if user has valid session
+  if (!session) {
+    res.status(401).json({
+      error: "Unauthorized access!",
+    });
+    return;
+  }
+
   if (req.method === "DELETE") {
     try {
-      const session = await getServerSession(req, res, authOptions);
-
-      // Check if user has valid session
-      if (!session) {
-        res.status(401).json({
-          error: "Unauthorized access!",
-        });
-        return;
-      }
-
       // Check if book id is provided
       const { id } = req.body;
       if (!id) {
@@ -41,16 +42,6 @@ const handler = async (req, res) => {
     }
   } else if (req.method === "PATCH") {
     try {
-      const session = await getServerSession(req, res, authOptions);
-
-      // Check if user has valid session
-      if (!session) {
-        res.status(401).json({
-          error: "Unauthorized access!",
-        });
-        return;
-      }
-
       // Check if book id is provided
       const { id } = req.body;
       if (!id) {
@@ -69,6 +60,39 @@ const handler = async (req, res) => {
 
       return res.status(201).json({
         message: "Book status updated",
+      });
+    } catch (e) {
+      res.status(500).json({
+        error: e.message,
+      });
+    }
+  } else if (req.method === "POST") {
+    try {
+      const { id, bookTitle, bookAuthor } = req.body;
+
+      if (!bookTitle.trim() || !bookAuthor.trim()) {
+        return res.status(422).json({
+          error: "Book title and author can't be empty",
+        });
+      }
+
+      await connectDB();
+
+      const book = await Book.findById(id);
+
+      if (!book) {
+        return res.status(404).json({
+          error: "This book doesn't exist!",
+        });
+      }
+
+      // Update book
+      book.title = bookTitle;
+      book.author = bookAuthor;
+      await book.save();
+
+      return res.status(200).json({
+        message: "Book has been updated",
       });
     } catch (e) {
       res.status(500).json({
