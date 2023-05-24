235f9360dcd642dad0702db2856e7b7edf9b43d6
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-05-24 20:48:12 +0300

    Fetch book data and pass down as props

--- src/pages/admin/[id]/edit.tsx
+++ src/pages/admin/[id]/edit.tsx
@@ -1,8 +1,10 @@
 import React from "react";
+import type { GetServerSideProps } from "next";
 import { useRouter } from "next/router";
+import Book from "lib/db/models/book_model";
+import { Types } from "mongoose";
 
 import EditBookForm, { EditBookFormProps } from "components/EditBookForm";
-import mongoose from "mongoose";
 
 const EditPage = (props: EditBookFormProps) => {
   const router = useRouter();
@@ -24,3 +26,35 @@ const EditPage = (props: EditBookFormProps) => {
 };
 
 export default EditPage;
+
+export const getServerSideProps: GetServerSideProps = async (contx) => {
+  const id = contx.query.id.toString();
+
+  if (!Types.ObjectId.isValid(id)) {
+    return {
+      redirect: {
+        destination: "/admin",
+        permanent: false,
+      },
+    };
+  }
+  const bookData = await Book.findById(id);
+
+  if (!bookData) {
+    return {
+      redirect: {
+        destination: "/admin",
+        permanent: false,
+      },
+    };
+  }
+  const bookJSON = JSON.stringify(bookData);
+  const book = JSON.parse(bookJSON);
+
+  return {
+    props: {
+      ...book,
+      id: book._id,
+    },
+  };
+};
