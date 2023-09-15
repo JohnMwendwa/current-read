47472b64a6ba6aebc36079ecaa8a3a2c2d837e97
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-16 01:10:51 +0300

    create and render the WishList component

---
+++ src/pages/wish-list/index.tsx
@@ -0,0 +1,12 @@
+import WishList from "components/WishList";
+
+function WishListPage() {
+  return (
+    <div>
+      <h1 className="text-2xl text-center font-bold mb-4">My Book Wish List</h1>
+      <WishList />
+    </div>
+  );
+}
+
+export default WishListPage;
