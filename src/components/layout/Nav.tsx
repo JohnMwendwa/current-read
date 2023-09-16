49dd3bc7f9052219b547fc3b5612be9d86040d6e
John Mwendwa <72663882+JohnMwendwa@users.noreply.github.com>
2023-09-16 13:53:57 +0300

    make navbar to be fixed at the top

--- src/components/layout/Nav.tsx
+++ src/components/layout/Nav.tsx
@@ -5,25 +5,28 @@ import { signIn, signOut, useSession } from "next-auth/react";
 const Nav = () => {
   const { data: session } = useSession();
   return (
-    <header className="flex justify-between items-center px-6 py-4 bg-blue-700/80 text-white shadow-md ">
+    <header className="flex justify-between items-center px-6 py-4 bg-blue-700/80 text-white shadow-xl backdrop-blur-sm fixed top-0 right-0 left-0     z-10">
       <Link href="/" className="font-extrabold text-2xl">
         JM
       </Link>
       <nav className="flex items-center space-x-4">
         <Link
           href={"/wish-list"}
-          className="bg-white text-black px-4 py-2 rounded-lg hover:bg-gray-200"
+          className="bg-white hidden sm:inline-block text-black px-4 py-2 rounded-lg hover:bg-gray-200"
         >
           Wish
         </Link>
         {session?.user ? (
           <>
-            <Link href="/admin" className="hover:text-gray-200">
+            <Link
+              href="/admin"
+              className="bg-blue-700 px-4 py-2 rounded-lg hover:text-gray-200"
+            >
               Admin
             </Link>
             <button
               onClick={() => signOut()}
-              className="bg-red-600 px-3 py-1 rounded-lg hover:bg-red-700"
+              className="bg-red-600 px-3 py-2 rounded-lg hover:bg-red-700"
             >
               Logout
             </button>
